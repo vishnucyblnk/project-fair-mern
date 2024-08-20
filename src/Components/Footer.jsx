624a9739f6f49a8bdae12d5d5bea0d18a2d6dc35
VISHNU NAIR B <0880vishnu@gmail.com>
2024-08-20 08:47:08 +0530

    footer-updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaStackOverflow } from "react-icons/fa";
+import { FaEnvelope, FaGithub, FaInstagram, FaLinkedin, FaStackOverflow } from "react-icons/fa";
 
 function Footer() {
     return (
         <div className='d-flex flex-column align-items-center justify-content-center mt-2' style={{ backgroundColor: '#90ee90', zIndex: '1', width: '100%', height: '400px' }}>
-            <div className='footer d-flex justify-content-evenly w-100'>
+            <div className='footer d-flex flex-wrap justify-content-evenly w-100'>
                 <div style={{ width: '400px' }} className='website'>
                     <h4 className='d-flex fw-bold'>
                         <FaStackOverflow />
@@ -28,10 +28,10 @@ function Footer() {
                 </div>
                 <div className='guide d-flex flex-column '>
                     <h4>Guides</h4>
-                    <Link to={'https://react.dev/'} style={{ textDecoration: 'none', color: '#444' }}>React</Link>
-                    <Link to={'https://react-bootstrap.netlify.app/'} style={{ textDecoration: 'none', color: '#444' }}>React Bootstrap</Link>
-                    <Link to={'https://www.w3schools.com/react/react_router.asp'} style={{ textDecoration: 'none', color: '#444' }}>Routing </Link>
-                    <Link to={'https://redux-toolkit.js.org/'} style={{ textDecoration: 'none', color: '#444' }}>Redux-Toolkit </Link>
+                    <Link to={'https://react.dev/'} target='_blank' style={{ textDecoration: 'none', color: '#444' }}>React</Link>
+                    <Link to={'https://react-bootstrap.netlify.app/'} target='_blank' style={{ textDecoration: 'none', color: '#444' }}>React Bootstrap</Link>
+                    <Link to={'https://www.w3schools.com/react/react_router.asp'} target='_blank' style={{ textDecoration: 'none', color: '#444' }}>Routing </Link>
+                    <Link to={'https://redux-toolkit.js.org/'} target='_blank' style={{ textDecoration: 'none', color: '#444' }}>Redux-Toolkit </Link>
 
                 </div>
                 <div>
@@ -39,11 +39,15 @@ function Footer() {
                     <input className='p-1 rounded' type="email" placeholder='enter your email id' />
                     <button className='btn btn-primary p-1 ms-3 '>Subscribe</button>
                     <div className='m-4 icons '>
-                        <i className="fa-regular fa-envelope fa-xl p-3" style={{ color: '' }}></i>
-                        <i className="fa-brands fa-instagram fa-xl p-3" style={{ color: '' }}></i>
-                        <i className="fa-brands fa-twitter fa-xl p-3" style={{ color: '' }}></i>
-                        <i className="fa-brands fa-facebook fa-xl p-3" style={{ color: '' }}></i>
-                        <i className="fa-brands fa-linkedin-in fa-xl p-3" style={{ color: '' }}></i>
+                        <Link className='p-3 text-dark text-decoration-none ' to="https://github.com/vishnucyblnk" target="_blank" rel="noopener noreferrer">
+                            <FaGithub size={50} />
+                        </Link>
+                        <Link className='p-3 text-dark text-decoration-none ' to="https://www.instagram.com/vishnunair_b/" target="_blank" rel="noopener noreferrer">
+                            <FaInstagram size={50} />
+                        </Link>
+                        <Link className='p-3 text-dark text-decoration-none ' to="https://www.linkedin.com/in/vishnu-nair-b-dev" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin size={50} />
+                        </Link>
                     </div>
                 </div>
             </div>
